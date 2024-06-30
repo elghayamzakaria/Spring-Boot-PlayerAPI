cf0bce0423de316ea78f7e56bcfd73288275e0d8
Spring <z.elghayam@gmail.com>
2024-06-30 12:19:58 +0100

    Add Player

--- src/main/java/com/spring/player/controller/PlayerController.java
+++ src/main/java/com/spring/player/controller/PlayerController.java
@@ -1,10 +1,13 @@
 package com.spring.player.controller;
 
+import com.spring.player.model.Player;
 import com.spring.player.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 //http://localhost:8080
 @Controller
@@ -26,7 +29,14 @@ public class PlayerController {
 
     //http://localhost:8080/player-form
     @GetMapping("/player-form")
-    public String playerForm(){
+    public String playerForm(Model model){
+        model.addAttribute("player", new Player());
         return "playerform";
+    }
+
+    @PostMapping("/savePlayer")
+    public String savePlayer(@ModelAttribute("player") Player player){
+        playerService.savePlayer(player);
+        return "redirect:/players";
     }
 }
